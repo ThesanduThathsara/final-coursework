c01a8d39f2b312a182c3d207513d5d85a8fe2f7e
ThesanduThathsara <thesandu1602500@gmail.com>
2025-08-29 22:41:14 +0530

    update

--- app/index.tsx
+++ app/index.tsx
@@ -3,7 +3,7 @@ import { Text, View } from "react-native";
 export default function Index() {
   return (
     <View className='flex-1 items-center justify-center bg-black'>
-      <Text className="text-xl font-bold text-white">Edit app/index.tsx to edit this screen.</Text>
+      <Text className="text-xl font-bold text-white">Welcome</Text>
     </View>
   );
 }
