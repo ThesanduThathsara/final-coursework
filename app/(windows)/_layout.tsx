620e734f38235b13759f81fa04b884b616b9d7bd
ThesanduThathsara <thesandu1602500@gmail.com>
2025-09-05 18:30:52 +0530

    bug fixing

--- app/(windows)/_layout.tsx
+++ app/(windows)/_layout.tsx
@@ -41,13 +41,13 @@ const _layout = () => {
                 },
                 tabBarStyle:{
                     backgroundColor:'#0f0D23',
-                    boarderRadius:50,
+                    borderRadius:50,
                     marginHorizontal:20,
                     marginBottom:36,
                     height:52,
                     position:"absolute",
                     overflow:'hidden',
-                    boarderWidth:1,
+                    borderWidth:1,
                     borderColor:'#0f0D23',
                 }
             }}
