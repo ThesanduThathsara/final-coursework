3105f43926a584a0031d31d064bb003079dd77c3
ThesanduThathsara <thesandu1602500@gmail.com>
2025-09-05 18:50:24 +0530

    change the tab bar width

--- app/(windows)/_layout.tsx
+++ app/(windows)/_layout.tsx
@@ -12,7 +12,7 @@ const TabiIcon = ({focused, icon, title}: any) => {
                 source={images.highlight}
                 // className="flex flex-row w-full flex-1 min-w[112px] min-h-14 mt-4 justify-center items-center
                 // rounded-full overflow-hidden"
-                className="flex flex-row w-[120px] min-w-[112px] min-h-16 mt-4 justify-center items-center rounded-full overflow-hidden"
+                className="flex flex-row w-full min-w-[112px] min-h-16 mt-4 justify-center items-center rounded-full overflow-hidden"
             >
                 <Image source={icon}
                        tintColor="#151312" className="size-5"/>
