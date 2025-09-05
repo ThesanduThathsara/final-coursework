bf7975c3dc7bd8baf04d3c28fbf51869a06be813
ThesanduThathsara <thesandu1602500@gmail.com>
2025-09-05 18:27:38 +0530

    create the tab bar

--- app/(windows)/_layout.tsx
+++ app/(windows)/_layout.tsx
@@ -5,58 +5,103 @@ import {images} from "@/assets/constants/images";
 import {icons} from "@/assets/constants/icons";
 
 // create component
-const TabiIcon=()=>{
-    return(
-        <ImageBackground
-            source={images.highlight}
-            // className="flex flex-row w-full flex-1 min-w[112px] min-h-14 mt-4 justify-center items-center
-            // rounded-full overflow-hidden"
-            className="flex flex-row w-[120px] min-w-[112px] min-h-14 mt-4 justify-center items-center rounded-full overflow-hidden"
-        >
-            <Image source={icons.home}
-                   tintColor="#151312" className="size-5"/>
+const TabiIcon = ({focused, icon, title}: any) => {
+    if (focused) {
+        return (
+            <ImageBackground
+                source={images.highlight}
+                // className="flex flex-row w-full flex-1 min-w[112px] min-h-14 mt-4 justify-center items-center
+                // rounded-full overflow-hidden"
+                className="flex flex-row w-[120px] min-w-[112px] min-h-16 mt-4 justify-center items-center rounded-full overflow-hidden"
+            >
+                <Image source={icon}
+                       tintColor="#151312" className="size-5"/>
 
-            <Text className="text-secondary text-base font-semibold" >Home</Text>
-        </ImageBackground>
+                <Text className="text-black text-secondary text-base font-semibold ml-2">{title}</Text>
+            </ImageBackground>
+        )
+    }
+    return (
+        <View className="size-full justify-center items-center mt-4 rounded-full">
+            <Image source={icon} tintColor="#A8B5DB" className="size-5"/>
+        </View>
     )
 }
 
 const _layout = () => {
-  return (
-    <Tabs>
-        <Tabs.Screen
-            name="index"
-            options={{
-                title: 'Home',
-                headerShown: false,
-                tabBarIcon:({ focused }) => (
-                    <TabiIcon/>
-                )
+    return (
+        <Tabs
+            screenOptions={{
+                tabBarShowLabel:false,
+                tabBarItemStyle:{
+                    width:'100%',
+                    height:'100%',
+                    justifyContent:'center',
+                    alignItems: 'center',
+                },
+                tabBarStyle:{
+                    backgroundColor:'#0f0D23',
+                    boarderRadius:50,
+                    marginHorizontal:20,
+                    marginBottom:36,
+                    height:52,
+                    position:"absolute",
+                    overflow:'hidden',
+                    boarderWidth:1,
+                    borderColor:'#0f0D23',
+                }
             }}
-        />
-        <Tabs.Screen
-            name="search"
-            options={{
-                title: 'Search',
-                headerShown: false,
-            }}
-        />
-        <Tabs.Screen
-            name="saved"
-            options={{
-                title: 'Saved',
-                headerShown: false,
-            }}
-        />
-        <Tabs.Screen
-            name="profile"
-            options={{
-                title: 'Profile',
-                headerShown: false,
-            }}
-        />
-    </Tabs>
-  );
+        >
+            <Tabs.Screen
+                name="index"
+                options={{
+                    title: 'Home',
+                    headerShown: false,
+                    tabBarIcon: ({focused}) => (
+                        <TabiIcon focused={focused}
+                                  icon={icons.home}
+                                  title="Home"/>
+                    )
+                }}
+            />
+            <Tabs.Screen
+                name="search"
+                options={{
+                    title: 'Search',
+                    headerShown: false,
+                    tabBarIcon: ({focused}) => (
+                        <TabiIcon focused={focused}
+                                  icon={icons.search}
+                                  title="Search"/>
+                    )
+                }}
+            />
+            <Tabs.Screen
+                name="saved"
+                options={{
+                    title: 'Saved',
+                    headerShown: false,
+                    tabBarIcon: ({focused}) => (
+                        <TabiIcon focused={focused}
+                                  icon={icons.save}
+                                  title="Saved"/>
+                    )
+                }}
+            />
+            <Tabs.Screen
+                name="profile"
+                options={{
+                    title: 'Profile',
+                    headerShown: false,
+                    tabBarIcon: ({focused}) => (
+                        <TabiIcon focused={focused}
+                                  icon={icons.person}
+                                  title="Profile"/>
+                    )
+                }}
+            />
+        </Tabs>
+    );
 };
 
 const styles = StyleSheet.create({});
